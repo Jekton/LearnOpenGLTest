084653abe0ff455c43271b2d8a7d24cead3adfdd
luojitong <luojitong@bytedance.com>
2023-10-09 20:48:10 +0800

    3.5 shaders exercise 2

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,13 @@ static const auto sVertexShaderSource = R"(
 layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
 layout (location = 1) in vec3 aColor; // the color variable has attribute position 1
 
+uniform float offset;
+
 out vec3 ourColor; // output a color to the fragment shader
 
 void main()
 {
-    gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0);
+    gl_Position = vec4(aPos.x + offset, aPos.y, aPos.z, 1.0);
     ourColor = aColor; // set ourColor to the input color we got from the vertex data
 }
 )";
@@ -76,6 +78,8 @@ int main() try {
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     GLProgram program{sVertexShaderSource, sFragmentShaderSource};
+    program.use();
+    program.setUniform("offset", 0.4f);
 
     window.eventLoop([&program, vertexArray](Window* w) {
         processInput(*w);
